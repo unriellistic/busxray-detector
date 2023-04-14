b4605ff6c4954b0a34bc2affb0d05a0c2a4d29c0
unriellistic <charliewindle@gmail.com>
2023-04-14 11:31:45 +0800

    added a slicing-capable detector

--- tridentnet_predictor.py
+++ tridentnet_predictor.py
@@ -1,16 +1,31 @@
-from detectron2.config import CfgNode as CN
 from detectron2.config import get_cfg
 from detectron2.engine import DefaultPredictor
+from detectron2.structures import Instances
 
 from tridentnet import add_tridentnet_config
-from utils import parse_inference
+from utils import slice_image
+
+def parse_detectron2_inference(inference: Instances) -> list[dict]:
+    """
+    Converts the raw detectron2 inference format into a more readable format.
+    """
+    parsed = []
+    fields = inference.get_fields()
+    for i in range(len(inference)):
+        parsed.append({
+            "bbox": fields["pred_boxes"].tensor.cpu().numpy().tolist()[i],
+            "score": fields["scores"].cpu().numpy().tolist()[i],
+            "pred_class": fields["pred_classes"].cpu().numpy().tolist()[i]
+        })
+    
+    return parsed
 
 class TridentNetPredictor(DefaultPredictor):
-    '''
+    """
     Predictor class for TridentNet object detection.
     Callable; takes an OpenCV-format image as input, and outputs COCO-format predictions.
     See the parent class, DefaultPredictor, for more details.
-    '''
+    """
     def __init__(self, config_file: str, opts: list[str], confidence_threshold: float = 0.5):
         # get default detectron2 config
         cfg = get_cfg()
@@ -30,5 +45,38 @@ class TridentNetPredictor(DefaultPredictor):
     def __call__(self, original_image):
         raw_predictions = super().__call__(original_image)
         # process the predictions into a JSON-compatible format
-        predictions = parse_inference(raw_predictions["instances"])
+        predictions = parse_detectron2_inference(raw_predictions["instances"])
+        return predictions
+    
+class TridentNetSlicePredictor(TridentNetPredictor):
+    """
+    TridentNetPredictor that slices the image into smaller pieces first, for better detection of small objects.
+    """
+    def __init__(self, config_file: str, opts: list[str], confidence_threshold: float = 0.5, segment_size: int = 640, 
+                 overlap_portion: float = 0.5):
+        super().__init__(config_file, opts, confidence_threshold)
+        self.segment_size = segment_size
+        self.overlap_portion = overlap_portion
+
+    def preprocess(self, original_image):
+        # Preprocessing step: slices the image into multiple slices, for feeding to the model
+        slices = slice_image(original_image, self.segment_size, self.overlap_portion)
+        return slices
+    
+    def __call__(self, original_image):
+        slices = self.preprocess(original_image)
+        predictions = []
+        for imgslice in slices:
+            slice_preds = super().__call__(imgslice.img) # bboxes are (x1, y1, x2, y2)
+            for pred in slice_preds:
+                # add x-offset
+                pred["bbox"][0] += imgslice.x_offset
+                pred["bbox"][2] += imgslice.x_offset
+                # add y-offset
+                pred["bbox"][1] += imgslice.y_offset
+                pred["bbox"][3] += imgslice.y_offset
+
+                # add to the final predictions
+                predictions.append(pred)
+
         return predictions
--- utils.py
+++ utils.py
@@ -1,14 +1,51 @@
-from detectron2.structures import Instances
+import numpy as np
 
-def parse_inference(inference: Instances) -> list[dict]:
-    '''Convert the raw detectron2 inference format into a more readable format.'''
-    parsed = []
-    fields = inference.get_fields()
-    for i in range(len(inference)):
-        parsed.append({
-            "bbox": fields["pred_boxes"].tensor.cpu().numpy().tolist()[i],
-            "score": fields["scores"].cpu().numpy().tolist()[i],
-            "pred_class": fields["pred_classes"].cpu().numpy().tolist()[i]
-        })
-    
-    return parsed
+class ImageSlice:
+    """
+    Class for an image slice from slice_image.
+    Contains the image itself, and the x and y coordinates of the slice's origin.
+    """
+    def __init__(self, x_offset: int, y_offset: int, img: np.ndarray):
+        self.x_offset = x_offset
+        self.y_offset = y_offset
+        self.img = img
+
+def slice_image(img: np.ndarray, segment_size: int = 640, overlap_portion: float = 0.5) -> list[ImageSlice]:
+    """
+    Slices an image of any dimension into pieces of 640 by 640 with a specified overlap percentage.
+
+    Args:
+    img (np.ndarray): The image to be sliced.
+    overlap_portion (float): The amount of overlap between adjacent slices (0 to 1).
+
+    Returns a list of ImageSlice.
+    """
+    height, width = img.shape[:2]
+
+    # Calculate the number of rows and columns required to segment the image
+    overlap_pixels = int(segment_size * overlap_portion)
+    segment_stride = segment_size - overlap_pixels
+    num_rows = int(np.ceil((height - segment_size) / segment_stride)) + 1
+    num_cols = int(np.ceil((width - segment_size) / segment_stride)) + 1
+
+    slices = []
+
+    for row in range(num_rows):
+        for col in range(num_cols):
+            y_start = row * segment_stride
+            y_end = y_start + segment_size
+            x_start = col * segment_stride
+            x_end = x_start + segment_size
+
+            # Check if the remaining section of the image is less than segment_size. If so, move the slice back
+            if y_end > height:
+                y_end = height
+                y_start = height - segment_size
+            if x_end > width:
+                x_end = width
+                x_start = width - segment_size
+
+            imgslice = ImageSlice(x_start, y_start, img[y_start:y_end, x_start:x_end])
+            slices.append(imgslice)
+
+    return slices
